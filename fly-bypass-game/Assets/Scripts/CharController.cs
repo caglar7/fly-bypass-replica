e68be7513fd610b30b8548cc3433b349adff9a04
caglar7 <mcaglarcnkr@gmail.com>
2022-01-04 03:28:51 +0300

    main character model painted

--- fly-bypass-game/Assets/Scripts/CharController.cs
+++ fly-bypass-game/Assets/Scripts/CharController.cs
@@ -4,11 +4,21 @@ using UnityEngine;
 
 public class CharController : MonoBehaviour
 {
+    // char controller and speed adjusting
     private CharacterController charController;
-    public float speed = 5f;
+    [SerializeField] private float speed = 5f;
 
-    public float rotationSpeed = 50f;
+    // rotation parameters
+    [SerializeField] private float rotationSpeed = 50f;
     private float rotation = 0f;
+
+    // gravity, ground check, jump height
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private float jumpHeight = 10f;
+    private float gravity = -40f;
+    private float velocityY;
+    private bool isOnGround;
+    
 
     void Start()
     {
@@ -18,18 +28,30 @@ public class CharController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // check ground and apply gravity if not on ground
+        isOnGround = Physics.CheckSphere(transform.position, .1f, groundLayer, QueryTriggerInteraction.Ignore);
+        if (isOnGround && velocityY < 0f)
+            velocityY = 0f;
+        else
+            velocityY += gravity * Time.deltaTime;
+
         // to the left or right, for now later with screen scroll
         float horizontal = Input.GetAxisRaw("Horizontal");
         rotation = rotationSpeed * horizontal;
 
-        // rotation
+        // rotation 
         float rotationY = transform.rotation.eulerAngles.y;
         float angle = rotationY + rotation * Time.deltaTime;
         transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-        // move on direction obtained from rotation
+        // jump
+        if (isOnGround && Input.GetButtonDown("Jump"))
+            velocityY += Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+        // get movingVector from direction and custom gravity
         Vector3 movingDir = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
-        charController.Move(movingDir * speed * Time.deltaTime);
+        Vector3 movingVector = new Vector3(movingDir.x * speed, velocityY, movingDir.z * speed);
+        charController.Move(movingVector * Time.deltaTime);
 
     }
 }
